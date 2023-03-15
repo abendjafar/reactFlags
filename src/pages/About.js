290ce6d822fe3c81fcd5a884d71e2d1fe3c3f635
Abendjafar <texabdel@gmail.com>
2023-03-15 13:45:59 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+        <Logo />
+        <Navigation />
+            <h1>A propos</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia repudiandae unde hic id fuga dicta, natus obcaecati facilis aperiam. Assumenda reprehenderit perferendis accusantium, tenetur ex repudiandae labore et facere incidunt possimus modi autem officia consequatur at. Labore beatae quasi enim neque. In earum optio ad ipsum distinctio totam cumque soluta esse laudantium minima quo ex, ipsam voluptas blanditiis sit rem iure error doloremque unde maiores a corrupti delectus eos nisi! Nemo harum pariatur autem quasi minima voluptatum nihil, atque eos error molestiae accusantium ducimus temporibus eveniet non? Ullam quidem velit porro officiis dolore quibusdam eligendi, illum vero provident nostrum animi?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni neque laboriosam, obcaecati quibusdam nulla mollitia cupiditate tempore commodi id cum saepe dolorem quidem totam facere veritatis temporibus dolorum! Commodi, sit. Nihil cum hic recusandae eius? Aut aperiam molestias quasi cumque, voluptate, soluta mollitia veritatis neque, sapiente eius suscipit cum delectus voluptas repudiandae libero! Assumenda mollitia iste est vero aperiam magnam quo iusto dolorem natus iure dicta veritatis dolorum veniam voluptate ipsa laborum, reiciendis nulla quos distinctio sapiente incidunt earum! Enim provident cumque nemo dolores qui culpa illum itaque illo inventore aperiam eum labore, similique iure exercitationem quod cupiditate nostrum repudiandae! Ratione assumenda deleniti recusandae sequi architecto dicta tempora iure! Non aspernatur, voluptate iste at qui dolores dolorem fuga, ipsam molestias asperiores et commodi aut minus incidunt dolorum vel ipsum, debitis deserunt corporis nulla vitae facilis consequuntur fugiat iusto? Consequuntur atque, suscipit id voluptatem at nesciunt provident in sint dicta ea esse consequatur, veritatis, iste nihil iusto ullam quasi numquam ipsum facilis veniam illo? Repellendus illum saepe tempora quas numquam molestiae, incidunt autem minus aperiam voluptates architecto porro veniam eligendi, quod quidem qui! Reiciendis cupiditate, expedita saepe vero libero quis. Ut rem voluptas laboriosam at, praesentium nulla officiis esse quibusdam eligendi!</p>
+        </div>
+    );
+};
+
+export default About;
